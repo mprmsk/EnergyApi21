705c00be3222d6cc23ed9c0983acd15e38307a06
mprmsk <99753265+mprmsk@users.noreply.github.com>
2022-02-21 20:59:08 +0530

    Add files via upload

---
+++ Models/EnergyModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EnergyAPI.Models
+{
+    public class EnergyModel
+    {
+        public int id { get; set; }
+        public DateTime EnteredDate { get; set; }
+        public string Energy { get; set; }
+        public double Cost { get; set; }
+    }
+}
